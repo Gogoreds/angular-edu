ea32918df472d3c883741c4d68814456371fc464
Mo Arefi <m-arefi@live.com>
2023-03-15 15:09:38 +0100

    Neating up the place

--- angularUtbildning/src/app/admin/admin.module.ts
+++ angularUtbildning/src/app/admin/admin.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Containers
 import { ProductListComponent } from './containers/product-list/product-list.component';
+import { ProductSingleComponent } from './containers/product-single/product-single.component';
+
+// Components
 import { ProductCardComponent } from './components/product-card/product-card.component';
 
+// Services
+
+// Guards
+
+// Directives
+
 @NgModule({
-  declarations: [ProductListComponent, ProductCardComponent],
+  declarations: [ProductListComponent, ProductSingleComponent, ProductCardComponent],
   imports: [CommonModule],
-  exports: [ProductListComponent],
+  exports: [ProductListComponent, ProductSingleComponent],
 })
 export class AdminModule {}
---
+++ angularUtbildning/src/app/admin/containers/product-single/product-single.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-single',
+  template: `
+    <p>
+      product-single works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProductSingleComponent {
+
+}
