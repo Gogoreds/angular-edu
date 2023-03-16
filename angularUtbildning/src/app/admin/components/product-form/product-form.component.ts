8698dcf2be11a42fb1fa3ce9e72cbcf5801cc384
Mo Arefi <m-arefi@live.com>
2023-03-16 13:20:46 +0100

    Radio buttons and data binding

--- angularUtbildning/src/app/admin/components/product-form/product-form.component.ts
+++ angularUtbildning/src/app/admin/components/product-form/product-form.component.ts
@@ -12,10 +12,46 @@ import { Component } from '@angular/core';
         <span>Price</span>
         <input type="number" name="price" class="input" ngModel/>
       </label>
+
+      <div class="product-form-radios">
+        <p class="product-form-radios-label">Promo:</p>
+        <label class="">
+          <input class="" type="radio" name="promo" [value]="undefined" ngModel/>
+          <span>None</span>
+        </label>
+        <label class="">
+          <input class="" type="radio" name="promo" value="new" ngModel/>
+          <span>New</span>
+        </label>
+        <label class="">
+          <input class="" type="radio" name="promo" value="limited" ngModel/>
+          <span>Limited</span>
+        </label>
+      </div>
+
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
   styles: [
+  `
+    .product-form {
+      &-radios {
+        display: flex;
+        align-content: center;
+        &-label {
+          margin-right: 10px;
+        }
+        label {
+          display: flex;
+          align-items: center;
+          span {
+            color: #666;
+            margin-bottom: 0;
+          }
+        }
+      }
+    }
+  `
   ]
 })
 export class ProductFormComponent {
