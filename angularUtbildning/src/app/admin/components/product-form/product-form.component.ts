0a47b8bb09393a855f8327aa019a0479da3808de
Mo Arefi <m-arefi@live.com>
2023-03-16 08:47:46 +0100

    Styling and form component

--- angularUtbildning/src/app/admin/components/product-form/product-form.component.ts
+++ angularUtbildning/src/app/admin/components/product-form/product-form.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-product-form',
   template: `
     <form class="product-form" #form="ngForm">
-      <pre>
-        {{ form.value | json }}
-      </pre>
+      <label>
+        <span>Name</span>
+        <input type="text" name="name" class="input" ngModel/>
+      </label>
+      <label>
+        <span>Price</span>
+        <input type="number" name="price" class="input" ngModel/>
+      </label>
+      <pre>{{ form.value | json }}</pre>
     </form>
   `,
   styles: [
